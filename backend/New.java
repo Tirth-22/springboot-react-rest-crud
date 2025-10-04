5fcfd53873abd254173e9977bbabc1c934a21ca4
Tirth-22 <tirthmakadia2006@gmail.com>
2025-10-04 20:56:36 +0530

    initial commit

---
+++ backend/New.java
@@ -0,0 +1,9 @@
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") entityIdType id) {
+        try {
+            repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
