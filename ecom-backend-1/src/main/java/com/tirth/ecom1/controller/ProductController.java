dbfbfb11584d7cea7f64f27da07f82ea07553e24
Tirth-22 <tirthmakadia2006@gmail.com>
2025-10-05 03:37:40 +0530

    set for UI 2&3

--- ecom-backend-1/src/main/java/com/tirth/ecom1/controller/ProductController.java
+++ ecom-backend-1/src/main/java/com/tirth/ecom1/controller/ProductController.java
@@ -1,4 +1,46 @@
 package com.tirth.ecom1.controller;
 
+import com.tirth.ecom1.model.Product;
+import com.tirth.ecom1.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+       return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = service.getAllProductsById(id);
+        if(product == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else
+            return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            Product product1 = service.addProduct(product,imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
