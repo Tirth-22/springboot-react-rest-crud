e3c72d0c573533c978ec37110c8facc7c556e067
Tirth-22 <tirthmakadia2006@gmail.com>
2025-10-06 00:27:27 +0530

    all services done

---
+++ ecom-backend/src/main/java/com/tirth/ecom1/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.tirth.ecom1.service;
+
+import com.tirth.ecom1.model.Product;
+import com.tirth.ecom1.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getAllProductsById(int id) {
+        return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepo.save(product);
+    }
+
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setId(id);
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+
+        return  productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
+}
